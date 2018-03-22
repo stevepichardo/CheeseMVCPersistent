60d102c805045e3178030a177c6e40e04befcf22
stevepichardo <stevepichardo@outlook.com>
2018-03-21 23:55:10 -0400

    "fully working project.. part 3 is finished"

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,115 @@
+﻿using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller 
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Menu> menus = context.Menus.ToList();
+
+            return View(menus);
+        }
+
+        public IActionResult Add()
+        {
+            AddMenuViewModel addMenuViewModel = new AddMenuViewModel();
+            return View(addMenuViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name,
+
+                };
+
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            }
+
+            return View(addMenuViewModel);
+        }
+
+        public IActionResult ViewMenu(int id)
+        {
+            Menu viewMenu = context.Menus.Single(c => c.ID == id);
+
+            List<CheeseMenu> items = context
+                .CheeseMenus
+                .Include(item => item.Cheese)
+                .Where(cm => cm.MenuID == id)
+                .ToList();
+
+            ViewMenuViewModel viewMenuViewModel = new ViewMenuViewModel()
+            {
+                Menu = viewMenu,
+                Items = items
+            };
+
+            return View(viewMenuViewModel);
+        }
+
+        public IActionResult AddItem(int id)
+        {
+            Menu newMenuItem = context.Menus.Single(m => m.ID == id);
+            List<Cheese> cheeses = context.Cheeses.ToList();
+
+            AddMenuItemViewModel addMenuItemViewModel =
+                new AddMenuItemViewModel(newMenuItem, cheeses);
+
+            return View(addMenuItemViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddItem(AddMenuItemViewModel addMenuItemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IList<CheeseMenu> existingItems = context.CheeseMenus
+                    .Where(cm => cm.CheeseID == addMenuItemViewModel.cheeseID)
+                    .Where(cm => cm.MenuID == addMenuItemViewModel.menuID).ToList();
+
+                if (existingItems.Count == 0)
+                {
+                    CheeseMenu newCheeseMenu = new CheeseMenu()
+                    {
+                        MenuID = addMenuItemViewModel.menuID,
+                        CheeseID = addMenuItemViewModel.cheeseID
+                    };
+
+                    context.CheeseMenus.Add(newCheeseMenu);
+                    context.SaveChanges();
+
+                    return Redirect("/Menu/ViewMenu/" + newCheeseMenu.MenuID);
+                }
+                return Redirect("/Menu/Index");
+
+            }
+
+            return View(addMenuItemViewModel);
+        }
+    }
+
+}
--- src/CheeseMVC/Models/Menu.cs
+++ src/CheeseMVC/Models/Menu.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace CheeseMVC.Models
 {
-    public class Cheese
+    public class Menu
     {
+        public int ID{ get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public int ID { get; set; }
-
-        public int CategoryID { get; set; }
-        public CheeseCategory Category { get; set; }
 
         public IList<CheeseMenu> CheeseMenus { get; set; }
     }
